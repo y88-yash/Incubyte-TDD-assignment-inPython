6360b1cf7d949a96b996d557cb2c1a79e44e78ab
Yash shah <yashshah8450@gmail.com>
2022-08-21 19:27:29 +0530

    Second test case fail
    If string is 1 then its return its value but its not returning any value

--- String_Calculator.py
+++ String_Calculator.py
@@ -2,3 +2,5 @@
 def add(number):
     if len(number.strip()) == 0:
         return 0
+    elif len(number) == 1:
+        return 
--- TestStringcalculator.py
+++ TestStringcalculator.py
@@ -6,3 +6,7 @@ class TestStringcalculator(unittest.TestCase):
     def test_empty_string(self):
         '''It should return 0 if the string is empty'''
         self.assertEqual(String_Calculator.add(""),0)
+
+    def test_single_string(self):
+        '''It should return 1 if the string is 1'''
+        self.assertEqual(String_Calculator.add("1,2"),1)
