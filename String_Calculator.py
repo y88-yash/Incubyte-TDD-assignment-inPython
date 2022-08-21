eaff3413c40fd088c0c2d33116fe0742bf03b95c
Yash shah <yashshah8450@gmail.com>
2022-08-21 19:51:03 +0530

    Refactoring the all the cases

--- String_Calculator.py
+++ String_Calculator.py
@@ -2,10 +2,10 @@
 def add(number):
     if len(number.strip()) == 0:
         return 0
-    elif len(number) == 1:
-        return int(number)
+    # elif len(number) == 1:
+    #     return int(number)
     
-    elif len(number) >= 2:
+    elif len(number) >= 1:
         token=number.split(",")
         sumoftwostring=list(map(int,token))
         sum=0
