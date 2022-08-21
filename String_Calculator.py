e7210a8d5da5c40a97bf41e2d97161c0a0b252ca
Yash shah <yashshah8450@gmail.com>
2022-08-21 19:46:25 +0530

    Third test case fail
    If the second string or multi string are given by user then its return the sum of that string but its not return any value

--- String_Calculator.py
+++ String_Calculator.py
@@ -4,3 +4,11 @@ def add(number):
         return 0
     elif len(number) == 1:
         return int(number)
+    
+    elif len(number) >= 2:
+        token=number.split(",")
+        sumoftwostring=list(map(int,token))
+        sum=0
+        for i in sumoftwostring:
+            sum += i
+        return 
--- TestStringcalculator.py
+++ TestStringcalculator.py
@@ -9,4 +9,8 @@ class TestStringcalculator(unittest.TestCase):
 
     def test_single_string(self):
         '''It should return 1 if the string is 1'''
-        self.assertEqual(String_Calculator.add("1"),1)
+        self.assertEqual(String_Calculator.add("5"),5)
+    
+    def test_double_string(self):
+        '''It should return the sum of given string like 1+2=3'''
+        self.assertEqual(String_Calculator.add("1,2"),4)
